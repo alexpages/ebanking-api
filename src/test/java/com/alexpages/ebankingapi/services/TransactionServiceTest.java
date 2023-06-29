5c64c6f3e2d35836126b8edecdf2a90070a6070a
Alex Pages <alexpagesandreu@gmail.com>
2023-06-29 17:52:59 +0200

    Refactor

--- src/test/java/com/alexpages/ebankingapi/services/TransactionServiceTest.java
+++ src/test/java/com/alexpages/ebankingapi/services/TransactionServiceTest.java
@@ -3,14 +3,10 @@ package com.alexpages.ebankingapi.services;
 import com.alexpages.ebankingapi.models.client.ClientRepository;
 import com.alexpages.ebankingapi.models.transaction.Currency;
 import com.alexpages.ebankingapi.models.transaction.Transaction;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import java.util.*;
@@ -21,18 +17,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TransactionServiceTest {
 
     @Mock private KafkaTemplate<String, String> kafkaTemplate;
-    @Mock private Consumer<String, String> kafkaConsumer;
     @Mock private ClientService clientService;
-    @Mock private ExchangeRateService exchangeRateService;
-    @Mock private Calendar calendar;
-    @Mock private ObjectMapper objectMapper;
     @Mock private ClientRepository clientRepository;
 
     @InjectMocks
     private TransactionService underTest;
 
     @Test
-    public void itShouldPublishTransaction() throws JsonProcessingException {
+    public void itShouldPublishTransaction(){
         // Given
         Transaction transaction = Transaction.builder()
                 .date(new Date())
